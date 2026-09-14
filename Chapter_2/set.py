1b5c7ded71eaccdef0c89f93789de0e12af65bc3
Atulsaini2806 <atulsaini10123@gamil.com>
2026-09-14 23:44:39 +0530

    Set

---
+++ Chapter_2/set.py
@@ -0,0 +1,19 @@
+# Set is a collection of unordered items.(no chnage or no indexing) and represent in {}.
+# Each element in the set muet be unique and immutable.
+# set = mutuable
+# set -> element -> immutable
+
+set = {1,2,3,4,5,6 }
+set1 = {1,2,2,3,3,"hello", "hello","bye"}
+print(set) # {1, 2, 3, 4, 5, 6}
+print(type(set)) # <class 'set'>
+print(set1) # {1, 2, 3, 'bye', 'hello'} return set without duplicate element.
+
+
+collection = {}
+print(type (collection)) #<class 'dict'> 
+
+# collection1 = set{} 
+# print(type(collection1))
+
+
