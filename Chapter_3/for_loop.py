96b59e4f091e6a03b17d981e6a7795c70869dcda
Atulsaini2806 <atulsaini10123@gamil.com>
2026-09-19 18:02:41 +0530

    for loop

---
+++ Chapter_3/for_loop.py
@@ -0,0 +1,28 @@
+# for loop are used for sequential traversal for traveresing list,string.tuples.
+
+#example :-
+list = [1,2,3,4,5,6]
+for el in list:
+    print(el)
+else:
+    print("END")
+
+#example 2:-
+str = "apnacollage"
+for char in str:
+    if(char == 'o'):
+        print("O found")
+        break
+        print(char)
+
+# Search for anumner x in this tuple using for loop
+nums = (1,4,9,16,25,36,49,64,81,100)
+x = 36
+idx = 0
+for el in nums:
+    if(el == x):
+        print("num found", idx)
+        idx+1
+        break
+    else:
+        print("num does not exit")  
