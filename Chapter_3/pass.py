627e0d8203d807573100f0371cf768a8f8384f25
Atulsaini2806 <atulsaini10123@gamil.com>
2026-09-21 22:37:20 +0530

    Pass statement

---
+++ Chapter_3/pass.py
@@ -0,0 +1,13 @@
+# Pass statement:-
+# pass statament is a null statement that does nothing.  it is used as a placeholder for the future code.
+
+for i in range(5):
+    # empty
+    print(i) # it wil  give an error
+
+# But if we used passs statment:-
+for i in range(5):
+    pass
+    print(i)
+
+# Note:- Pass statement is completely differnet from the continue.
